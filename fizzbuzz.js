b7be1c5517500c492477aaf20686591d13bc37a0
Raman <raman.balyan@gmail.com>
2016-03-11 10:15:43 +0530

    fizzbuzz program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,5 @@
-/* Write a program that uses console.log to print all the numbers 
+/* PROBLEM:
+Write a program that uses console.log to print all the numbers 
 from 1 to 100, with three exceptions. For numbers divisible by 3, 
 print "Fizz" instead of the number, and for numbers divisible by 5
 (and not 3), print "Buzz" instead. Also, print "FizzBuzz", for numbers 
